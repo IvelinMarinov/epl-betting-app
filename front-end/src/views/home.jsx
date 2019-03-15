584b3307edf7c49d9643fadafb18a55f0c20dcf6
IvelinMarinov <ivelin.marinov@gmail.com>
2019-03-15 20:54:31 +0200

    favicon and html title; home page content

--- front-end/src/views/home.jsx
+++ front-end/src/views/home.jsx
@@ -10,9 +10,15 @@ class Home extends Component {
                 <h3 className="text-center">Welcome to our betting site</h3>
                 <hr />
                 
-                <p className="text-center">Rules.......................................</p>
-                <p className="text-center">More Rules..............</p>
-                <p className="text-center">Even More Rules..............</p>
+                <p className="text-center">EPL Betting is a free, non-commercial website. This is the perfect office game among colleagues.</p>
+                <p className="text-center"><strong><u>In order to use EPL Betting you must be at least 18 years old.</u></strong></p>
+                <br/>
+                <p className="text-center">Each user can bet on the final result of each English Preamier League game.</p>
+                <p className="text-center">Each guessed sign (1, 2 or X) brings you <strong>1 point</strong>. If you guess the exact result of a game, you get <strong>3 points</strong></p>
+                <br/>
+                <p className="text-center">The winner is picked at the end of the season. There are no rewards guys, sorry :)</p>
+
+
 
                 {/* <img src={EplLogo('./premier-league-logo-background.png')}  /> */}
             </div>
